de9d93611fdc3c3fefca5a33dff3aa35a29168c5
Benedikt Kirchmeier <seriousbenedikt@web.de>
2018-09-19 09:21:45 +0200

    Better physics

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -73,8 +73,8 @@ public class FirstPersonController : MonoBehaviour
     [SerializeField] Vector3 gunGrabExtents;
     [SerializeField] Transform gunHolder;
 
-    private Rigidbody m_RigidBody;
-    private CapsuleCollider m_Capsule;
+    private Rigidbody rb;
+    private CapsuleCollider capColl;
     private float m_YRotation;
     private Vector3 m_GroundContactNormal;
     private bool m_Jump, m_PreviouslyGrounded, m_Jumping, m_IsGrounded;
@@ -82,7 +82,7 @@ public class FirstPersonController : MonoBehaviour
 
     public Vector3 Velocity
     {
-        get { return m_RigidBody.velocity; }
+        get { return rb.velocity; }
     }
 
     public bool Grounded
@@ -107,8 +107,8 @@ public class FirstPersonController : MonoBehaviour
 
     private void Start()
     {
-        m_RigidBody = GetComponent<Rigidbody>();
-        m_Capsule = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
+        capColl = GetComponent<CapsuleCollider>();
         mouseLook.Init (transform, cam.transform);
         input = GetComponent<PlayerInput>();
         equippedGun = GetComponentInChildren<GunController>();
@@ -161,33 +161,52 @@ public class FirstPersonController : MonoBehaviour
             desiredMove.x = desiredMove.x*movementSettings.CurrentTargetSpeed;
             desiredMove.z = desiredMove.z*movementSettings.CurrentTargetSpeed;
             desiredMove.y = desiredMove.y*movementSettings.CurrentTargetSpeed;
-            if (m_RigidBody.velocity.sqrMagnitude <
-                (movementSettings.CurrentTargetSpeed*movementSettings.CurrentTargetSpeed))
+
+            // Get the velocity
+            Vector3 horizontalMove = desiredMove;
+            // Don't use the vertical velocity
+            horizontalMove.y = 0f;
+            // Calculate the approximate distance that will be traversed
+            float distance = horizontalMove.magnitude * Time.fixedDeltaTime;
+            // Normalize horizontalMove since it should be used to indicate direction
+            horizontalMove.Normalize();
+            RaycastHit hit;
+
+            // Check if the body's current velocity will result in a collision
+            if (rb.SweepTest(horizontalMove, out hit, distance))
             {
-                m_RigidBody.AddForce(desiredMove*SlopeMultiplier(), ForceMode.Impulse);
+                if(hit.point.y > transform.position.y - capColl.height * 0.25f)
+                {
+                    // If so, stop the movement
+                    rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+                }
+            }
+            else if (rb.velocity.sqrMagnitude < (movementSettings.CurrentTargetSpeed*movementSettings.CurrentTargetSpeed))
+            {
+                rb.AddForce(desiredMove*SlopeMultiplier(), ForceMode.Impulse);
             }
         }
 
         if (m_IsGrounded)
         {
-            m_RigidBody.drag = 5f;
+            rb.drag = 5f;
 
             if (m_Jump)
             {
-                m_RigidBody.drag = 0f;
-                m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0f, m_RigidBody.velocity.z);
-                m_RigidBody.AddForce(new Vector3(0f, movementSettings.JumpForce, 0f), ForceMode.Impulse);
+                rb.drag = 0f;
+                rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+                rb.AddForce(new Vector3(0f, movementSettings.JumpForce, 0f), ForceMode.Impulse);
                 m_Jumping = true;
             }
 
-            if (!m_Jumping && Mathf.Abs(inputDir.x) < float.Epsilon && Mathf.Abs(inputDir.y) < float.Epsilon && m_RigidBody.velocity.magnitude < 1f)
+            if (!m_Jumping && Mathf.Abs(inputDir.x) < float.Epsilon && Mathf.Abs(inputDir.y) < float.Epsilon && rb.velocity.magnitude < 1f)
             {
-                m_RigidBody.Sleep();
+                rb.Sleep();
             }
         }
         else
         {
-            m_RigidBody.drag = 0f;
+            rb.drag = 0f;
             if (m_PreviouslyGrounded && !m_Jumping)
             {
                 StickToGroundHelper();
@@ -231,13 +250,13 @@ public class FirstPersonController : MonoBehaviour
     private void StickToGroundHelper()
     {
         RaycastHit hitInfo;
-        if (Physics.SphereCast(transform.position, m_Capsule.radius * (1.0f - advancedSettings.shellOffset), Vector3.down, out hitInfo,
-                                ((m_Capsule.height/2f) - m_Capsule.radius) +
+        if (Physics.SphereCast(transform.position, capColl.radius * (1.0f - advancedSettings.shellOffset), Vector3.down, out hitInfo,
+                                ((capColl.height/2f) - capColl.radius) +
                                 advancedSettings.stickToGroundHelperDistance, Physics.AllLayers, QueryTriggerInteraction.Ignore))
         {
             if (Mathf.Abs(Vector3.Angle(hitInfo.normal, Vector3.up)) < 85f)
             {
-                m_RigidBody.velocity = Vector3.ProjectOnPlane(m_RigidBody.velocity, hitInfo.normal);
+                rb.velocity = Vector3.ProjectOnPlane(rb.velocity, hitInfo.normal);
             }
         }
     }
@@ -270,7 +289,7 @@ public class FirstPersonController : MonoBehaviour
         {
             // Rotate the rigidbody velocity to match the new direction that the character is looking
             Quaternion velRotation = Quaternion.AngleAxis(transform.eulerAngles.y - oldYRotation, Vector3.up);
-            m_RigidBody.velocity = velRotation*m_RigidBody.velocity;
+            rb.velocity = velRotation*rb.velocity;
         }
     }
 
@@ -279,8 +298,8 @@ public class FirstPersonController : MonoBehaviour
     {
         m_PreviouslyGrounded = m_IsGrounded;
         RaycastHit hitInfo;
-        if (Physics.SphereCast(transform.position, m_Capsule.radius * (1.0f - advancedSettings.shellOffset), Vector3.down, out hitInfo,
-                                ((m_Capsule.height/2f) - m_Capsule.radius) + advancedSettings.groundCheckDistance, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+        if (Physics.SphereCast(transform.position, capColl.radius * (1.0f - advancedSettings.shellOffset), Vector3.down, out hitInfo,
+                                ((capColl.height/2f) - capColl.radius) + advancedSettings.groundCheckDistance, Physics.AllLayers, QueryTriggerInteraction.Ignore))
         {
             m_IsGrounded = true;
             m_GroundContactNormal = hitInfo.normal;
